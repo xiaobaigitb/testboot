4c01efe022b72c253ec0077417e19422a6b8c7d5
xiaobaigitb <xiaobai_13520@163.com>
2021-01-12 16:38:55 +0800

    ““SpringBoot相关记录”

---
+++ target/classes/static/sql/testOne.sql
@@ -0,0 +1,3 @@
+INSERT INTO sql_user(username,PASSWORD) VALUES("A","123456");
+INSERT INTO sql_user(username,PASSWORD) VALUES("B","123456");
+INSERT INTO sql_user(username,PASSWORD) VALUES("C","123456");
